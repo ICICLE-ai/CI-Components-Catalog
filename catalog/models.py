398e5257d478be0b2c47758fa2664238a313698f
jasonthekim <jasonthekim2002@gmail.com>
2023-02-27 01:01:38 -0600

    updated models.py script to read YAML data into neo4j, need to test with pod later

--- catalog/models.py
+++ catalog/models.py
@@ -1,9 +1,47 @@
 import yaml
-
-# testing things
+from neo4j import GraphDatabase
+# testing things 123 123 
 
 # Path to the dataset file.
-DATASET = '/catalog/components-data.yaml'
+DATASET = '../components-data.yaml'
+
+# Connect to the Neo4j database.
+uri = "bolt://localhost:7687"
+driver = GraphDatabase.driver(uri, auth=("username", "password"))
+
+# Load the component data from the YAML file.
+with open(DATASET, 'r') as f:
+    data = yaml.safe_load(f)
+
+# Define the Cypher queries to create the nodes and relationships.
+component_query = """
+CREATE (:Component {id: $id, name: $name, version: $version, public_access: $public_access, restricted_to_role: $restricted_to_role})
+"""
+
+dependency_query = """
+MATCH (c1:Component {id: $from_id}), (c2:Component {id: $to_id})
+CREATE (c1)-[:DEPENDS_ON]->(c2)
+"""
+
+# Define a function to execute the queries.
+def load_data(tx, data):
+    # Create the component nodes.
+    for component in data['components']:
+        tx.run(component_query, id=component['id'], name=component['name'], version=component['version'], public_access=component['publicAccess'], restricted_to_role=component.get('restrictedToRole'))
+    # Create the dependency relationships.
+    for dependency in data['dependencies']:
+        tx.run(dependency_query, from_id=dependency['from'], to_id=dependency['to'])
+
+# Execute the queries in a transaction.
+with driver.session() as session:
+    session.write_transaction(load_data, data)
+Note that this is just an example script, and you may need to modify it to fit your specific use case.
+
+
+
+
+
+
 
 
 def get_components():
@@ -40,3 +78,4 @@ def filter_components_by_roles(components, user_roles):
             result.append(c)
     return result
 
+def 
