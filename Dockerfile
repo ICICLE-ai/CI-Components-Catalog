f2dd116d36cf19a6f380c6043990f3c9e886e4a1
guzman109 <guzman.109@osu.edu>
2023-09-19 13:43:32 -0400

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,14 @@
 # Image: tapis/ci-catalog
 
 FROM python:3.10
+
 RUN pip install Flask==2.2
 RUN pip install pyyaml==6.0
 RUN pip install requests==2.28.2
 RUN pip install tapipy==1.2.20
 RUN pip install neo4j
 RUN pip install pytest
+RUN pip install iciflaskn
 
 # application directory
 RUN mkdir /catalog
@@ -20,4 +22,5 @@ ADD catalog /catalog
 
 WORKDIR /catalog
 
+ENV APP_CONFIG_PATH /catalog/config.yaml
 CMD ["python", "app.py"]
--- catalog/app.py
+++ catalog/app.py
@@ -2,36 +2,18 @@ from flask import Flask, render_template, redirect, request
 import json
 import requests
 
-
-import auth
 from config import config
 import logging
 import models
 
+from iciflaskn import icicle_flaskn, auth
+
 app = Flask(__name__)
+app.register_blueprint(icicle_flaskn)
 app.logger.setLevel(logging.INFO)
 
 # Check config:
 # sanity checks
-if 'client_id' not in config:
-    raise Exception("no client_id in config. Quitting..")
-else: 
-    app.logger.info(f"using client_id: {config['client_id']}")
-
-if 'client_key' not in config or config['client_key'] == None:
-    raise Exception("no client_key in config. Quitting..")
-else: 
-    app.logger.info(f"using client_key: ********")
-    
-if 'tapis_base_url' not in config:
-    raise Exception("no tapis_base_url in config. Quitting..")
-else: 
-    app.logger.info(f"using tapis_base_url: {config['tapis_base_url']}")
-
-if 'app_base_url' not in config:
-    raise Exception("no app_base_url in config. Quitting..")
-else: 
-    app.logger.info(f"using app_base_url: {config['app_base_url']}")
 
 # Log the neo4j backend flag
 if 'neo4j_backend' not in config:
@@ -52,70 +34,12 @@ if 'neo4j_url' not in config:
     raise Exception("no neo4j_url in config. Quitting..")
 else: 
     app.logger.info(f"using neo4j_url: {config['neo4j_url']}")
-    
-# Log the tenant
-if 'tenant' not in config:
-    raise Exception("no tenant in config. Quitting..")
+
 
     
 # Set the secret key to some random bytes. 
 # TODO: this key should be updated
 app.secret_key = b'dsckj32487sj238193626%^#$'
-
-
-@app.route('/logout', methods=['GET'])
-def logout():
-    auth.clear_session()
-    return redirect('/data', code=302)
-
-
-@app.route('/login', methods=['GET'])
-def login():
-    """
-    Check for the existence of a login session, and if none exists, start the OAuth2 flow.
-    """
-    authenticated, _, _ = auth.is_logged_in()
-    # if already authenticated, redirect to the main data table
-    if authenticated:
-        return redirect("/data", code=302)
-    # otherwise, start the OAuth flow
-    callback_url = f"{config['app_base_url']}/oauth2/callback"
-    tapis_url = f"{config['tapis_base_url']}/v3/oauth2/authorize?client_id={config['client_id']}&redirect_uri={callback_url}&response_type=code"
-    return redirect(tapis_url, code=302)
-
-
-@app.route('/oauth2/callback', methods=['GET'])
-def callback():
-    """
-    Process a callback from a Tapis authorization server:
-      1) Get the authorization code from the query parameters.
-      2) Exchange the code for a token
-      3) Add the user and token to the session
-      4) Redirect to the /data endpoint. 
-    """
-    code = request.args.get('code')
-    if not code:
-        raise Exception(f"Error: No code in request; debug: {request.args}")
-    url = f"{config['tapis_base_url']}/v3/oauth2/tokens"
-    data = {
-        "code": code, 
-        "redirect_uri": f"{config['app_base_url']}/oauth2/callback", 
-        "grant_type": "authorization_code",
-    }
-    try:
-        response = requests.post(url, data=data, auth=(config['client_id'], config['client_key']))
-        response.raise_for_status()
-        json_resp = json.loads(response.text)
-        token = json_resp['result']['access_token']['access_token']
-    except Exception as e:
-        raise Exception(f"Error generating Tapis token; debug: {e}")
-
-    username = auth.get_username(token)
-    app.logger.info(f"Got username for token; username: {username}")
-    roles = auth.add_user_to_session(username, token)
-    app.logger.info(f"Username added to session; found these roles: {roles}")
-    return redirect("/data", code=302)    
-
 
 @app.route('/', methods=['GET'])
 def root():
